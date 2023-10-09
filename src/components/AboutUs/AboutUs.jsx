cfc70cb80b4995f5474d6efd1b664b2e377bbb83
Emiliano Rodriguez <emirod1955@gmail.com>
2023-10-08 23:21:09 -0300

    correccion link about us

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -23,7 +23,7 @@ const AboutUs = () =>{
                 <div className="textBlock">
                     <h1>Our challenge</h1>
                     <p>"Your challenge is to create an interactive, virtual reality application within a web browser that enables users to visualize a space mission including the mission concept, orbital trajectories, and mission timeline. Don’t forget to embed your space mission visualization app in a web page and deploy that web page on a free web hosting service so that your solution is available to all."</p>
-                    <Link to="https://www.spaceappschallenge.org/2023/challenges/visualize-a-space-mission-using-virtual-reality/"><input id='ourChallengeButton' type="button" value="Learn more" /></Link>
+                    <Link to="/demo"><input id='ourChallengeButton' type="button" value="Learn more" /></Link>
                 </div>
             </div>
         </div>
